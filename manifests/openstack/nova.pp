a5f4d8368744d8199f9803ac4598dbb3ed6fc750
hkumarmk <hkumarmk@gmail.com>
2014-06-26 08:16:02 +0000

    added missing variables as class args
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@673 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/openstack/nova.pp
+++ manifests/openstack/nova.pp
@@ -2,6 +2,7 @@
 
 class jiocloud::openstack::nova (
   $iam_compute_node = $jiocloud::params::iam_compute_node,
+  $iam_os_controller_node = $jiocloud::params::iam_os_controller_node,
   $nova_db_url = $jiocloud::params::nova_db_url,
   $nova_rpc_backend = $jiocloud::params::nova_rpc_backend,
   $nova_glance_api_servers = $jiocloud::params::nova_glance_api_servers,
@@ -19,12 +20,19 @@ class jiocloud::openstack::nova (
   $service_tenant = $jiocloud::params::service_tenant,
   $region = $jiocloud::params::region,
   $keystone_internal_url = $jiocloud::params::keystone_internal_url,
+  $nova_vncproxy_nodes = $jiocloud::params::nova_vncproxy_nodes,
+  $nova_api_nodes = $jiocloud::params::nova_api_nodes,
+  $nova_scheduler_nodes = $jiocloud::params::nova_scheduler_nodes,
+  $nova_consoleauth_nodes = $jiocloud::params::nova_consoleauth_nodes,
+  $conductor_nodes = $jiocloud::params::conductor_nodes,
+  $nova_cert_nodes = $jiocloud::params::nova_cert_nodes,
+  $cinder_volume_nodes = $jiocloud::params::cinder_volume_nodes,
+  $cinder_scheduler_nodes = $jiocloud::params::cinder_scheduler_nodes,
 ) {
-  
   if $iam_compute_node {
     class { 'jiocloud::openstack::nova::compute': }
   } elsif $iam_os_controller_node {
       class { 'jiocloud::openstack::nova::controller':}
   }  
-    
+  class { '::nova::zeromq': }
 }
