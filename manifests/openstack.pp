2fd9a3acaa991ab557b0fc8a147c4579a9964cc8
hkumarmk <hkumarmk@gmail.com>
2014-06-27 04:02:31 +0000

    only run openstack controller classes on controller nodes
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@676 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/openstack.pp
+++ manifests/openstack.pp
@@ -1,5 +1,10 @@
 ### class: jiocloud::openstack
-class jiocloud::openstack {
-
-  class {'jiocloud::openstack::nova':}
+class jiocloud::openstack (
+  $iam_os_controller_node = $jiocloud::params::iam_os_controller_node,
+) {
+  if $iam_os_controller_node {
+    class {'jiocloud::openstack::nova':}
+    class {'jiocloud::openstack::dashboard':}
+    class {'jiocloud::openstack::apache':}
+  }
 }
