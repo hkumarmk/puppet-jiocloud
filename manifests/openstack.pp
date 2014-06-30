ad0bbd5d86f0fc13edcc1a20a16ba550caf4cfd9
hkumarmk <hkumarmk@gmail.com>
2014-06-30 11:15:23 +0000

    run openstack::nova class on all machines
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@694 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/openstack.pp
+++ manifests/openstack.pp
@@ -1,10 +1,14 @@
 ### class: jiocloud::openstack
 class jiocloud::openstack (
   $iam_os_controller_node = $jiocloud::params::iam_os_controller_node,
+  $iam_os_compute_node = $jiocloud::params::iam_os_compute_node,
 ) {
   if $iam_os_controller_node {
-    class {'jiocloud::openstack::nova':}
+    class {'jiocloud::openstack::keystone':}
     class {'jiocloud::openstack::dashboard':}
     class {'jiocloud::openstack::apache':}
-  }
+    class {'jiocloud::openstack::cinder':}
+    class {'jiocloud::openstack::glance':}
+  } 
+  class {'jiocloud::openstack::nova':}
 }
