a5fc565524dee3a02bff3604ca9110069a566e62
amar266 <amar266@gmail.com>
2014-07-03 10:56:38 +0000

    added facter environment in jiocloud module
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@708 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- lib/facter/jiocloud.rb
+++ lib/facter/jiocloud.rb
@@ -7,3 +7,23 @@ if default_route_row =~ /^(\d+\.\d+\.\d+\.\d+)\s*([\w\d]+)$/
   Facter.add("default_gw_if") { setcode { default_gw_if } }
   Facter.add("default_gw_ip") { setcode { default_gw_ip } }
 end
+networks_production = Array["10.135.120.0/24","10.135.121.0/24","10.135.112.0/24","10.135.122.0/24","10.135.123.0/24"]
+networks_staging = Array["10.135.123.0/24"]
+#Get all interfaces
+all_network = Facter::Util::Resolution.exec('ip route list | grep link | awk "{printf \$1\" \"}"').chomp
+#convert string to array
+all_network = all_network.split(" ")
+#Check any of element is common in array ,if any common will be found then it will return false otherwise it will return true
+production =(networks_production & all_network).empty?
+staging =(networks_staging & all_network).empty?
+
+if production == false
+   Facter.add("environment") { setcode { "production" } }
+elsif staging == false 
+   Facter.add("environment") { setcode { "staging" } }
+else 
+   Facter.add("environment") { setcode { "virtual" } }
+end
+
+
+
