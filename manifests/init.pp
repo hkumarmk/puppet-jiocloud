5cdc80ff25105d8ba093d05b8c61ae40daac08cf
hkumarmk <hkumarmk@gmail.com>
2014-07-04 04:00:29 +0000

    run memcached class only on memcache servers
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@725 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,18 @@
 class jiocloud {
+## Load params
   class {'jiocloud::params':}
   Exec { path => $jiocloud::params::executable_path }
+## system ops
   class {'jiocloud::system':}
+## DB server and client setup
   class {'jiocloud::db':}
-  class {'jiocloud::openstack':}
+##Setup openstack on openstack nodes.
+  if $jiocloud::params::iam_os_controller_node or $jiocloud::params::iam_os_compute_node {
+    class {'jiocloud::openstack':}
+  }
 
+  if $jiocloud::params::iam_memcached_node == 'true' {
+    class {'jiocloud::memcached':}
+  }
+notify {"memcache - $jiocloud::params::iam_memcached_node":}
 }
