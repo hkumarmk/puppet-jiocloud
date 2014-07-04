9ea77985b5da27cc71ee0284aab2b46f3d2d97d5
hkumarmk <hkumarmk@gmail.com>
2014-07-04 00:42:16 +0000

    call more modules
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@718 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class jiocloud {
   class {'jiocloud::params':}
   Exec { path => $jiocloud::params::executable_path }
   class {'jiocloud::system':}
-  #class {'jiocloud::db':}
-  #class {'jiocloud::openstack':}
+  class {'jiocloud::db':}
+  class {'jiocloud::openstack':}
 
 }
