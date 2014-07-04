7e2dac850262d4d81c1885c74d6f3125ce116f0d
hkumarmk <hkumarmk@gmail.com>
2014-07-04 06:29:49 +0000

    fixed typo
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@738 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/openstack.pp
+++ manifests/openstack.pp
@@ -1,7 +1,7 @@
 ### class: jiocloud::openstack
 class jiocloud::openstack (
   $iam_os_controller_node = $jiocloud::params::iam_os_controller_node,
-  $iam_os_compute_node = $jiocloud::params::iam_os_compute_node,
+  $iam_compute_node = $jiocloud::params::iam_compute_node,
 ) {
   if $iam_os_controller_node {
     class {'jiocloud::openstack::keystone':}
@@ -10,7 +10,7 @@ class jiocloud::openstack (
     class {'jiocloud::openstack::cinder':}
     class {'jiocloud::openstack::glance':}
     class {'jiocloud::openstack::nova':}
-  } elsif $iam_os_compute_node { 
+  } elsif $iam_compute_node { 
   class {'jiocloud::openstack::nova':}
   }
 
