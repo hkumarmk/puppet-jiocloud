ba5abf35f6c05885f5eaa3fcbb523d91c92113b2
hkumarmk <hkumarmk@gmail.com>
2014-07-04 04:21:18 +0000

    added appropriate depandancies between classes
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@727 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,14 +5,19 @@ class jiocloud {
 ## system ops
   class {'jiocloud::system':}
 ## DB server and client setup
-  class {'jiocloud::db':}
+  class {'jiocloud::db':
+    require => Class['jiocloud::system'],
+  }
 ##Setup openstack on openstack nodes.
   if $jiocloud::params::iam_os_controller_node or $jiocloud::params::iam_os_compute_node {
-    class {'jiocloud::openstack':}
+    class {'jiocloud::openstack':
+      require => Class['jiocloud::system'],
+    }
   }
 
   if $jiocloud::params::iam_memcached_node == 'true' {
-    class {'jiocloud::memcached':}
+    class {'jiocloud::memcached':
+      require => Class['jiocloud::system'],
+    }
   }
-notify {"memcache - $jiocloud::params::iam_memcached_node":}
 }
