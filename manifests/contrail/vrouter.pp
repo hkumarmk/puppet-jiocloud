2a0f0f5e1979b90b9c834afb6b53e9c25056e353
hkumarmk <hkumarmk@gmail.com>
2014-07-30 20:52:11 +0000

    migrated vrouter setup code from contrail.pp to contrail/vrouter.pp
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@860 7e7508ed-7dfa-4282-9f22-b7e683a64a08

---
+++ manifests/contrail/vrouter.pp
@@ -0,0 +1,53 @@
+### Setting up contrail
+class jiocloud::contrail::vrouter ( 
+### Define variables Here
+  $iam_os_compute_node     =   $jiocloud::params::iam_compute_node,
+  $vrouter_interface       =   $jiocloud::params::vrouter_interface,
+  $vrouter_physical_interface = $jiocloud::params::vrouter_physical_interface,
+  $vrouter_ip              =   $jiocloud::params::contrail_vrouter_ip,
+  $vrouter_num_controllers = $jiocloud::params::contrail_vrouter_num_controllers,
+  $edge_router_address		   = $jiocloud::params::contrail_edge_router_address,
+  $discovery_server        =   $jiocloud::params::contrail_discovery_server,
+  $vrouter_mac             =   $jiocloud::params::contrail_vrouter_mac,
+  $vrouter_gw              =   $jiocloud::params::contrail_vrouter_gw,
+  $vrouter_cidr            =   $jiocloud::params::contrail_vrouter_cidr,
+  $metadata_proxy_shared_secret    = $jiocloud::params::nova_neutron_metadata_proxy_shared_secret,
+  $vrouter_file_source = $jiocloud::params::vrouter_file_source,
+  $vrouter_file = $jiocloud::params::vrouter_file,
+){
+##### Conditional application of role based classes
+  
+# if iam_os_compute_node {  
+#	class {'::role_contrail_vrouter':}
+#  }
+ class {'::contrail::vrouter':
+        vrouter_interface       => $vrouter_interface,
+        vrouter_ip              => $vrouter_ip,
+        discovery_server        => $discovery_server,
+        vrouter_mac             => $vrouter_mac,
+        vrouter_gw              => $vrouter_gw,
+        vrouter_cidr            => $vrouter_cidr,
+        vrouter_physical_interface => $vrouter_physical_interface,
+        metadata_proxy_shared_secret    => $metadata_proxy_shared_secret,
+	vrouter_num_controllers => $vrouter_num_controllers,
+ 	edge_router_address    => $edge_router_address
+   }
+
+
+  file { ['/lib/modules',"/lib/modules/${kernelrelease}","/lib/modules/${kernelrelease}/extra","/lib/modules/${kernelrelease}/extra/net","/lib/modules/${kernelrelease}/extra/net/vrouter"]:
+        ensure  => directory,
+        before  => File ["$vrouter_file"],
+  }
+  file { "$vrouter_file":
+    ensure        => file,
+    owner         => root,
+    group         => root,
+    mode          => 644,
+    source        => "$vrouter_file_source",
+  }
+
+## End of contrail_setup
+}
+
+  
+##### Setting up role based classes
