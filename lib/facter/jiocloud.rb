a6ad95e9a7cf2284d28809ec70fa96a33d9c7b2d
hkumarmk <hkumarmk@gmail.com>
2014-08-06 12:12:55 +0000

    migrated facter 'env' and other details to hiera/papply.env
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@891 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- lib/facter/jiocloud.rb
+++ lib/facter/jiocloud.rb
@@ -10,23 +10,3 @@ end
 jiocloud_release = Facter::Util::Resolution.exec('cat /etc/jiocloud-release')
 Facter.add("jiocloud_release") { setcode { jiocloud_release.chomp } }
 
-networks_production = Array["10.135.120.0/24","10.135.121.0/24","10.135.112.0/24","10.135.122.0/24","10.135.123.0/24"]
-networks_staging = Array["10.135.123.0/24"]
-#Get all interfaces
-all_network = Facter::Util::Resolution.exec('ip route list | grep link | awk "{printf \$1\" \"}"').chomp
-#convert string to array
-all_network = all_network.split(" ")
-#Check any of element is common in array ,if any common will be found then it will return false otherwise it will return true
-production =(networks_production & all_network).empty?
-staging =(networks_staging & all_network).empty?
-
-if production == false
-   Facter.add("env") { setcode { "production" } }
-elsif staging == false 
-   Facter.add("env") { setcode { "staging" } }
-else 
-   Facter.add("env") { setcode { "virtualcloud" } }
-end
-
-
-
