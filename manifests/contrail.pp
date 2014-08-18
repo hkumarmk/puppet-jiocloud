8bc1bf3ab76d627d1b70b77b0796831093065b55
hkumarmk <hkumarmk@gmail.com>
2014-08-18 03:58:06 +0000

    Made certain variable as parameters, call  contrail::database on contrail node
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/modules/jiocloud@910 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/contrail.pp
+++ manifests/contrail.pp
@@ -1,11 +1,13 @@
 ### Setting up contrail
 class jiocloud::contrail (
+  $configure_contrail_node = false,
+  $repo_type =  'apt-repo',
 ) inherits jiocloud::params {
   ## Setup contrail controller components on contrail_node
   if $iam_contrail_node and $configure_contrail_node {
     ## install contrail-install (repo) package and other base packages
     class {'::contrail::install':
-      contrail_repo_type => 'contrail-ubuntu-package'
+      contrail_repo_type => $repo_type,
     }    
     ## Do basic system configuration, which is required for contrail controller to work
     class {'::contrail::system_config': }
@@ -15,6 +17,8 @@ class jiocloud::contrail (
     class {'::contrail::haproxy': }
     ## Setup rabbitmq
     class {'::contrail::rabbitmq': }
+    ## Setup cassandra
+    class {'::contrail::database': }
   } elsif  $iam_compute_node {
     ## Setup vrouter on compute node
     class {'jiocloud::contrail::vrouter': }
